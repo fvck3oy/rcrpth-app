9ba87bed08dfe6eadefad422baf22dd1c2d1a3be
Pumin Swangjang <fvck3oy@hotmail.com>
2019-05-23 03:31:09 +0700

    build

---
+++ build/precache-manifest.a314009f41f2f3e9602712d58d0fbd93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585b8284546ef177552caf305159e9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0465131bcb6c95dbd5",
+    "url": "/static/css/2.7a5d1e2e.chunk.css"
+  },
+  {
+    "revision": "ee7f8aa3f092e7e71ffa",
+    "url": "/static/css/main.7329f8f0.chunk.css"
+  },
+  {
+    "revision": "ec0465131bcb6c95dbd5",
+    "url": "/static/js/2.7f9ff2ad.chunk.js"
+  },
+  {
+    "revision": "ee7f8aa3f092e7e71ffa",
+    "url": "/static/js/main.856276b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5722917342dac5c57e9994ddfe665bf8",
+    "url": "/static/media/bg1.57229173.png"
+  },
+  {
+    "revision": "0093dc9ce8b23b1ad3030a2120c9f577",
+    "url": "/static/media/news-1.0093dc9c.jpg"
+  }
+]);
